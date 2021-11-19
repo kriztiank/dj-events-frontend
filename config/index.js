1196e991fa8f9c94411e2298e9f6be3e707d1c21
kriztiank <kriztiank@gmail.com>
2021-11-19 11:04:58 +0100

    Login, Get JWT

--- config/index.js
+++ config/index.js
@@ -1,6 +1,11 @@
-// Development endpoint
+// Strapi Backend URL
 export const API_URL =
     process.env.NEXT_PUBLIC_API_URL || 'http://localhost:1337'
+ 
+// Next Frontend URL    
+export const NEXT_URL =
+    process.env.NEXT_PUBLIC_FRONTEND_URL || 'http://localhost:3000'
 
 // Pagination, items per page
 export const PER_PAGE = 2
+
